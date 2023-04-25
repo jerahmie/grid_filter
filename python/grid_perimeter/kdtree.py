092164d2c91b56e328dfa53043b7992453cf8f9b
Jerahmie Radder <radd0012@umn.edu>
2023-04-25 17:09:14 -0500

    first implementation of 2d kdtree with initial unittests.  Testing still in progress

--- python/grid_perimeter/kdtree.py
+++ python/grid_perimeter/kdtree.py
@@ -2,14 +2,69 @@
 """
 from typing import List, Tuple
 
-def sort_points(pts: List[Tuple[float, float]], idx: int) -> List[Tuple[float, float]]:
-    """Return a list of sorted points by index
+def sort_points(pts: List[Tuple[float, float]], dim: int) -> List[Tuple[float, float]]:
+    """Return a list of sorted points by dimension.
+
+    Keyword arguments:
+    pts -- list of (x, y) points 
+    dim -- dimension to sort over. (0 : x, 1 : y)
     """
-    pass
+    if dim == 0:
+        pts = sorted(pts, key=lambda pt: pt[0])
+    elif dim == 1:
+        pts = sorted(pts, key=lambda pt: pt[1])
+    else:
+        raise RuntimeError("Could not sort on index: {dim}".format(dim=dim))
+    return pts
+
+def median_point_id(pts: List[Tuple[float, float]]) -> Tuple[Tuple[float, float], int] :
+    """Return the index, point of the median point in list of points.
+
+    Keyword arguments:
+    pts -- list of (x, y) points
+    dim -- dimension to search for media (0 : x, 1 : y)
+    """
+    idx = int((len(pts) - 1) // 2) 
+
+    return pts[idx], idx
+
+class Node2D():
+    """KD-Tree Node"""
+    def __init__(self, data, left = None, right=None):
+        self.left = left
+        self.right = right
+        self.data = data
+
+    def __str__(self):
+        """Pretty-print the node data"""
+        return f'({self.data})'
+    
+    def __repr__(self):
+        """Define the repr string."""
+        return f'(Node2D, \'{self.data}\', left={self.left}, right={self.right})'
+
+def build_tree(pts, depth=0) -> Node2D:
+    """helper function to build the kd tee"""
+    if len(pts) == 1:
+        return Node2D(pts[0])
+    else:
+        (med, idx) = median_point_id(pts)
+        depth += 1
+        dim = depth%2
+        if idx > 0:
+            left_subtree = build_tree(sort_points(pts[:idx], dim), depth)
+        else: 
+            left_subtree = None
+        if (idx+1) < len(pts):
+            right_subtree = build_tree(sort_points(pts[idx+1:], dim), depth)
+        else:
+            right_subtree = None
+        return Node2D(med, left=left_subtree, right=right_subtree)
 
 class KDTree2D():
     """ 2-Dimensional KD Tree.
     """
     def __init__(self, pts: List[Tuple[float, float]]):
         self._points = pts
-
+        self._root = None
+    
--- test/test_kdtree.py
+++ test/test_kdtree.py
@@ -1,6 +1,6 @@
 """Unittests for kdtree module"""
 import unittest
-from grid_perimeter import KDTree2D
+from grid_perimeter import KDTree2D, sort_points, median_point_id, Node2D, build_tree
 
 class TestKDTree2D(unittest.TestCase):
     """Test case for 2D KD Tree"""
@@ -9,13 +9,59 @@ class TestKDTree2D(unittest.TestCase):
         pass
 
     def setUp(self):
-        pass
+        # a non-trivial list of points
+        self.p = [(1.1,-2.2), (3.3, 4.4), (-1.1, 2.2), (-6.6, 7.7)]
 
     def test_kdtree2d(self):
         """Test create kdtree"""
         p = [(1.1, 2.2), (3.3, 4.4)]
         a = KDTree2D(p)
         self.assertTrue(isinstance(a, KDTree2D))
+
+    def test_sort_points(self):
+        """Test ability to sort points over x- or y- points."""
+        spx = [(-6.6, 7.7), (-1.1, 2.2), (1.1, -2.2), (3.3, 4.4)]
+        spy = [(1.1,-2.2), (-1.1,2.2), (3.3, 4.4), (-6.6, 7.7)]
+
+        # single point should already be sorted
+        self.assertEqual([(1.1, 2.2)], sort_points([(1.1, 2.2)], 0))
+        self.assertEqual([(1.1, 2.2)], sort_points([(1.1, 2.2)], 1))
+
+        # empty list should return empty list
+        self.assertEqual([], sort_points([],0)) 
+        # sort multi-point list
+        self.assertEqual(spx, sort_points(self.p,0))
+        self.assertEqual(spy, sort_points(self.p,1))
+
+        # sorting over wrong index should raise exception
+        with self.assertRaises(RuntimeError):
+            sort_points(self.p,2)
+
+    def test_median_point_id(self):
+        """Test find media point from sorted list."""
+        self.assertEqual(((-1.1, 2.2), 1), median_point_id(sort_points(self.p, 0)))
+        p5 = self.p
+        p5.append((-10.0, -10.0))
+        self.assertEqual(((-1.1, 2.2), 2), median_point_id(sort_points(p5, 0)))
+        # single point list
+        self.assertEqual(((-1.1, 2.2), 0), median_point_id([(-1.1, 2.2)]))
+        self.assertEqual(((0, 0), 0), median_point_id([(0, 0)]))
+
+    def test_kdtree2d_node(self):
+        """Test the constuction of kdtree node"""
+        nroot = Node2D((1.0, 2.0))
+        self.assertTrue(isinstance(nroot, Node2D))
+        self.assertEqual('((1.0, 2.0))', str(nroot))
+        self.assertEqual('(Node2D, \'(1.0, 2.0)\', left=None, right=None)', repr(nroot))
+
+    def test_build_tree(self):
+        """Test the construction of a kd tree."""
+        pts = [(0.0, 1.1), (1.1, -1.0), (2.2, -1.1), (3.3, 4.4)]
+        #(med, idx) = median_point_id(pts)
+        #tree = build_tree(med)
+        a = build_tree(pts)
+        self.assertEqual("", repr(a.right))
+        #self.assertTrue(False)
 
     def tearDown(self):
         pass
