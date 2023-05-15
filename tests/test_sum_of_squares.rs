8ce7e7f62e0a1aa6ef130c6c4c693bae6e07b6bd
Jerahmie Radder <jerahmie@gmail.com>
2023-05-15 10:45:07 -0500

    added rust tests/ directory

---
+++ tests/test_sum_of_squares.rs
@@ -0,0 +1,14 @@
+use grid_perimeter::sum_of_squares::*;
+
+//fn sum_of_squares(arr: &[i32]) -> i32 {
+//    5
+//}
+
+#[cfg(test)]
+    #[test]
+    fn it_works() {
+        let arr: Vec<i32> = (0..10).collect();
+        //let result = sum_of_squares(arr).unwrap();
+        let result = sum_of_squares(&arr);
+        assert_eq!(result, 285);
+    }
