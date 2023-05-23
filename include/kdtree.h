645fe989a62d1d1185b4a4b283b152866d19c94c
Jerahmie Radder <radd0012@umn.edu>
2023-05-23 17:05:38 -0500

    Added CMakeLists.txt and added sanity check CTest

---
+++ include/kdtree.h
@@ -0,0 +1,6 @@
+#ifndef _KDTREE_H_
+#define _KDTREE_H_
+
+
+
+#endif /* _KDTREE_H_ */
---
+++ src/kdtree.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "kdtree.h"
+
+int main(void) {
+
+  std::cout << "Hello kdtree\n";
+}
