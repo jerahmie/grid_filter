74388dc00a314eaec4a34851671940248a86945b
jradder <jradder@mmmhattrick.mmm.ucar.edu>
2023-05-26 01:14:17 +0000

    moved python and c++ tests to test/ directory

--- python/grid_filter/__init__.py
+++ python/grid_filter/__init__.py
@@ -3,9 +3,9 @@ from .mpas_grid import MPASGrid
 from .read_regional_data import get_mpas_grid, get_grid_lat_lon
 from .grid_util import *
 from .graph_data import *
-from visualization.plot_grid import plot_mpas_grid, overplot_mpas_grid
 from .kdtree import *
-from visualization.kdtree_display import KDTreeDisplay
+#from visualization.plot_grid import plot_mpas_grid, overplot_mpas_grid
+#from visualization.kdtree_display import KDTreeDisplay
 
 
 #__doc__ = grid_perimeter.__doc__
