Fixed issue where observation points and mpas grid are using different units.

examples/filter_obs.py
@@ -60,13 +60,12 @@ def main(static_file: str, obs_file: str, save_file: str) -> None:
     # cell grid points (lat, lon)
     pts = mpg.cell_points()
     bdy_msk = mpg.bdy_mask_cells
-    print(len(bdy_msk))
     t3 = time.time()
     print(f"prepare cell_points: {t2-t1:.2f}")
     
     print('Build KDTree2D')
     # cell grid points with index (lat, lon, i)
-    ptsi = [(pts[i][0], pts[i][1], i) for i in range(len(pts))]
+    ptsi = [(180.0/np.pi*pts[i][0], 180.0/np.pi*pts[i][1], i) for i in range(len(pts))]
     kd2d = KDTree2D(ptsi)
     t4 = time.time()
     print(f"build kdtree: {t3-t2:.2f}")

examples/plot_obs.py
@@ -48,21 +48,42 @@ def plot_obs(latc: np.ndarray, lonc: np.ndarray, mask: np.ndarray = None) -> plt
         ax.plot(lonc[i], latc[i], color='blue', linestyle='None', marker='o', markersize=1, transform=ccrs.Geodetic())
     return ax
     
+def overplot_points(ax: plt.axes, lats: np.ndarray, lons: np.ndarray, color="red") -> plt.axes:
+    """Plot lats, lons over existing plt axes.
+
+    Keyword arguments:
+    ax -- matplotlib.pyplot axes 
+    lats -- latitiude points
+    lons -- longitude points
+    color -- custom color 
+    """
+    print(f"[overplot_points] np.shape(lats): {np.shape(lats)}")
+    print(f"[overplot_points] np.shape(lons): {np.shape(lons)}")
+    if len(lats) != len(lons):
+        sys.exit(f"Latitude {len(lats)} and Longitudes {len(lons)} not equal.")
+    for i in range(len(lats)):
+        ax.plot(lons[i], lats[i], color=color, linestyle='None', marker='o', markersize=1, transform=ccrs.Geodetic())
+
+    return ax
 
 def main(args)->None:
     """Plot Observations.
     """
     if not os.path.exists(args.filename):
         raise FileNotFoundError( errno.ENOENT, os.strerror(errno.ENOENT), args.filename)
-    print(type(args))
-    print(args.filename)
-    filter_mask = grid_filter.read_h5data(args.filename, 'DerivedValue', 'LAMDomainCheck')
+    #filter_mask = grid_filter.read_h5data(args.filename, 'DerivedValue', 'LAMDomainCheck')
     latc = grid_filter.read_h5data(args.filename, 'MetaData', 'latitude')
     lonc = grid_filter.read_h5data(args.filename, 'MetaData', 'longitude')
     
     #ax = plot_obs(latc, lonc, filter_mask)
     ax = plot_obs(latc, lonc)
     print("Saving filtered observation points.")
+    if args.static_file is not None:
+        mpg = grid_filter.MPASGrid(args.static_file)
+        pts = 180.0/np.pi*np.array(mpg.cell_points())
+        print(f"np.shape(pts): {np.shape(pts)} type: {type(pts)}")
+        overplot_points(ax, pts[:,0], pts[:,1])
+
     plt.savefig('plot_obs.png')
 
 if __name__ == "__main__":
@@ -70,7 +91,11 @@ if __name__ == "__main__":
                         prog='plot_obs',
                         description='Plot the filter observations.',
                         epilog='plot_obs')
-    parser.add_argument('filename')
+    parser.add_argument('filename',
+                        help="Observation file (HDF5)")
+    parser.add_argument('--static_file',
+                        help="Regional MPAS Grid file (NetCDF).",
+                        required=False)
     args = parser.parse_args()
     main(args)
     #grid_filter.plot_mpas_grid(cell_lat, cell_lon)