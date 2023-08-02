7c52754c01db573fcd4cc09ceab95a5db17a702a
Jerahmie Radder <jerahmie@gmail.com>
2023-08-02 09:28:05 -0500

    Updated KDTree constructor to use unique_ptr with proper move semantics.

--- include/kdtree.h
+++ include/kdtree.h
@@ -15,7 +15,7 @@ class KDTree {
     int depth;
     std::unique_ptr<nodeData> nd;
   public:
-    KDTree(std::unique_ptr<nodeData> nd) {this->nd = nd};
+    KDTree(std::unique_ptr<nodeData> nd) : nd(std::move(nd)){}
 
     ~KDTree()=delete;
     friend std::tuple<nodeData, int> median_point_id(const std::vector<nodeData>);
