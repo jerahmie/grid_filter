32d12962227e3c4339ece92ff3f51cae3bc0d960
Jerahmie Radder <jerahmie@gmail.com>
2023-08-28 08:44:12 -0500

    Adding error handling for H5 write obs filter mask.

--- include/obs_util.h
+++ include/obs_util.h
@@ -1,44 +1,57 @@
 /*
  * file - obs_util.h
+ * Utilities to read and preprocess observation data.
  */
 
 #pragma once
 #include <vector>
 #include <string>
 #include <typeinfo>
-#include "H5Cpp.h"
-#include "kdtree_util.h"
+#include <H5Cpp.h>
+#include <H5Exception.h>
 
+#include "kdtree_util.h"
 
 std::vector<float> read_h5data(std::string&, std::string, std::string);
 
 std::vector<point2D> read_obs_points(std::string&);
 
 template <typename T>
-void write_h5data_1d(std::string &filename, std::string &groupname,
+int write_h5data_1d(std::string &filename, std::string &groupname,
                   std::string &datasetname,  std::vector<T> &data) {
-  if constexpr (std::is_same<T, int>::value) {
-    auto data_type = H5::PredType::NATIVE_INT;
-    std::cout << "Data is of type 'int' \n";
-  } else if constexpr (std::is_same<T, float>::value) {
-    auto data_type = H5::PredType::NATIVE_FLOAT;
-    std::cout << "Data is of type 'float' \n";
-  } else if constexpr (std::is_same<T, double>::value) {
-    auto data_type = H5::PredType::NATIVE_DOUBLE; 
-    std::cout << "Data is of type 'double' \n";
-  } else {
-    std::cout << "Data type is unknown.\n";
+  
+  try {
+    
+    H5::H5File h5file = H5::H5File(filename, H5F_ACC_TRUNC);
+    H5::Group group = h5file.createGroup(groupname.c_str());
+    const hsize_t dims[1] {data.size()};
+    H5::DataSpace dataspace(1, dims);
+    
+    H5::DataSet dataset = group.createDataSet(datasetname,
+                                              H5::PredType::NATIVE_INT,
+                                              dataspace);
+    dataset.write(data.data(), H5::PredType::NATIVE_INT);
+    dataspace.close();
+    dataset.close(); 
+    h5file.close();
   }
-  //try {
-   H5::H5File h5file = H5::H5File(filename, H5F_ACC_TRUNC);
-   H5::Group group = h5file.createGroup(groupname.c_str());
-   const hsize_t dims[1] {data.size()};
-   H5::DataSpace dataspace(1, dims);
-   H5::DataSet dataset = group.createDataSet(datasetname, H5::PredType::NATIVE_INT, dataspace);
-   dataset.write(data.data(), H5::PredType::NATIVE_INT);
-   dataspace.close();
-   dataset.close(); 
-   h5file.close();
-  // }
+  // catch failure due to H5File operations
+  catch ( H5::FileIException error ) {
+    error.printErrorStack();
+    return -1;
+}
+
+  // catch failure due to DataSet operations
+  catch ( H5::DataSetIException error ) { 
+    error.printErrorStack();
+    return -1;
+    }
+  // catch failure due to DataSpace operations
+  catch ( H5::DataSpaceIException error ){
+    error.printErrorStack();
+    return -1;
+}
+
+    return 0;
 }
 
